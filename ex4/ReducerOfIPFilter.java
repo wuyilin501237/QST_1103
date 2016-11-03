4ed5f4ddba6c9250a2217686836e96fbdeb7cbb9
wuyilin501237 <1193720799@qq.com>
2016-11-03 16:47:05 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,18 @@
 package No4;
-
+import java.util.HashSet;
 import java.util.Scanner;
-
+import java.util.Set;
 public class ReducerOfIPFilter {
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
-		}
-	}
+		         Scanner scanner = new Scanner(System.in);
+		         //使用set集合存放ip，保证没有重复ip地址，方便统计
+		         Set<String> s = new HashSet<String>();
+		         while (scanner.hasNext()){
+		             String[] line = scanner.nextLine().split("\\t");
+		             //将ip添加进Set集合
+		             s.add(line[0]);
+		        }
+		         //直接输出集合ip的个数
+		         System.out.println(s.size());
+		     }
 }
